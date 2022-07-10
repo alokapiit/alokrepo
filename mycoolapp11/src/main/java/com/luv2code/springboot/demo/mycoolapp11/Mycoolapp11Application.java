59bfb99a3aa96bec9f5a2f6467dea960ae17ae45
alok3 <alok3@LAPTOP-IA961PRG>
2022-07-10 15:46:31 +0530

    Third Commit

---
+++ mycoolapp11/src/main/java/com/luv2code/springboot/demo/mycoolapp11/Mycoolapp11Application.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot.demo.mycoolapp11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Mycoolapp11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mycoolapp11Application.class, args);
+	}
+
+}
---
+++ mycoolapp11/src/test/java/com/luv2code/springboot/demo/mycoolapp11/Mycoolapp11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolapp11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mycoolapp11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
